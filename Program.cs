f59c2ca698a5246d73165951e2e68c7ab2efde9b
CherpakAndrii <andriycherpak.education@gmail.com>
2021-05-25 21:10:05 +0300

    Зробив скетч мейну. Назви класів будуть змінюватися. Можливо, для виведення буде навіть окремий клас.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main()
+        {
+            RTree tree = RTreeFactory.FillRTree("cow.obj");
+            // Byte[][][] image = tree.GetImage();  // тут я створюю матрицю пікселів та заповнюю її
+            // Picture.Reverse(image);  // на минулому кроці матриця вийшла перевернутою та віддзеркаленою, фіксимо це.
+            // Picture.ToFile(image);   // Макс, це твоє. Очевидно, виводимо в файл
+        }
+    }
+}
